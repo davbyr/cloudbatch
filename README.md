01225b1d5ee6838bb3ab5d8f658b15c853018ba4
davbyr <davidbyrne5950@gmail.com>
2022-09-28 23:50:31 +0000

    get and put functions added

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # gsbatch
-Python object for batching google cloud objects via gsutil.
+Python classes to help with batching functions over data that is obtained
+from google buckets using gsutil. 
+
+See gsbatch_tutorial_notebook for example useage.
+
+## Setup and Requirements
+
+
--- gsbatch/gsbatch.py
+++ gsbatch/gsbatch.py
@@ -1,14 +1,17 @@
 import xarray as xr
 import subprocess
 import numpy as np
+import os
+import os.path as path
+import glob
 
 class Gsbatch():
     
     def __init__(self, 
-                 gs_list = None, gs_components = None,
-                 gs_getdir = '',
-                 local_list = None, local_components = None,
-                 local_ext = '.nc',
+                 gs_list = None, 
+                 gs_components = None,
+                 gs_dir = None,
+                 search = 'remote',
                  batch_size=10
                 ):
 
@@ -17,18 +20,18 @@ class Gsbatch():
             n_components = len(gs_components)
             gs_files = self._make_files_from_components(gs_components)
         elif gs_list is not None:
+            if type(gs_list) is str:
+                gs_list = [gs_list]
             gs_files = gs_list
         else:
             raise Exception("You need to provide file_list or file_components")
             
-        # Define local files
-        if local_components is not None:
-            n_components = len(local_components)
-            local_files = self._make_files_from_components(local_components)
-        elif local_list is not None:
-            local_files = local_list
-        else:
-            local_files = None
+        # Add directory to file names if wanted
+        if gs_dir is not None:
+            gs_files = [path.join(gs_dir, fn) for fn in gs_files]
+            
+        # Check for wildcards
+        gs_files = self._expand_wildcards(gs_files, search)
         
         # Initialise batches
         self.current_file = 0
@@ -36,38 +39,37 @@ class Gsbatch():
         
         n_files = len(gs_files)
         
-        if local_files is not None:
-            if len(local_files) != n_files:
-                raise Exception('Number of GS Files does not match local files')
-        
         n_batches = np.ceil( n_files / batch_size ).astype(int)
         last_batch_size = n_files % batch_size
         
         self.n_batches = n_batches
         self.n_files = n_files
         self.gs_files = gs_files
-        self.local_files = local_files
         self.last_batch_size = last_batch_size
         self.batch_size = batch_size
-        self.gs_getdir = gs_getdir
+        self.is_last_batch = False
+        self.search = search
+        self.tmp_files = []
         
-        self._update_batch()
-        
-        # Set get script command
-        get_cmd = 'gsutil -m cp {0} {1}'
+        self._update_batch() 
         
         return
             
     def next_batch(self):
-        self.current_batch = self.current_batch + 1
-        self.current_file = self.current_file + self.batch_size
-        self._update_batch()
+        if self.current_batch < self.n_batches - 1:
+            self.current_batch = self.current_batch + 1
+            self.current_file = self.current_file + self.batch_size
+            self._update_batch()
+        else:
+            print('Final batch has been reached. Cannot increase index.')
         
     def prev_batch(self):
         if self.current_batch > 0:
             self.current_batch = self.current_batch - 1
             self.current_file = self.current_file - self.batch_size
             self._update_batch()
+        else:
+            print('This is the first batch. Cannot decrease index')
         
     def reset_batch(self):
         self.current_batch = 0
@@ -75,12 +77,31 @@ class Gsbatch():
         self._update_batch()
         return
     
-    def get_batch(self):
+    def get_batch(self, get_dir):  
         
-        return
+        # Create get command using gsutil and run from command line
+        get_cmd = 'gsutil -m cp '
+        for ff in self.gs_files_batch:
+            get_cmd = get_cmd + f' {ff}'
+        get_cmd += f' {get_dir}'
+        print(get_cmd)
+        subprocess.run(get_cmd, shell=True)
         
-    def put_batch(self):
-        return
+        # Save list of current temporary files
+        got_files = []
+        for ff in self.gs_files_batch:
+            got_files.append(path.join(get_dir, path.basename(ff)))
+            
+        self.tmp_files = self.tmp_files + got_files
+        
+    def put_batch(self, put_dir):  
+        # Create get command using gsutil and run from command line
+        put_cmd = 'gsutil -m cp '
+        for ff in self.gs_files_batch:
+            put_cmd = put_cmd + f' {ff}'
+        put_cmd += f' {get_dir}'
+        print(put_cmd)
+        subprocess.run(put_cmd, shell=True)
     
     def set_batch_size(self, batch_size):
         
@@ -90,7 +111,31 @@ class Gsbatch():
         
         self.n_batches = n_batches
         self.last_batch_size = last_batch_size
-    
+        self._update_batch()
+                        
+    def is_final_batch(self):
+        if self.current_batch == self.n_batches - 1:
+            return True
+        else:
+            return False
+        
+    def delete_tmp_files(self):
+        for ff in self.tmp_files:
+            os.remove(ff)
+        self.tmp_files = []
+            
+    def delete_batch_files(self):
+        for ff in self.gs_files_batch:
+            os.remove(ff)
+        self.gs_files_batch = []
+        
+    def summary(self):
+        out_str = ''
+        print(f'   Number of batches:     {self.n_batches}')
+        print(f'   Number of files:       {self.n_files}')
+        print('')
+        print(f'   Current batch number:  {self.current_batch+1}')
+        
     def _update_batch(self):
         
         start_idx = self.current_file
@@ -101,9 +146,6 @@ class Gsbatch():
             end_idx = self.current_file + self.last_batch_size
         
         self.gs_files_batch = self.gs_files[start_idx:end_idx]
-        
-        if self.local_files is not None:
-            self.local_files_batch = self.local_files[start_idx:end_idx]
     
     def _make_files_from_components(self, components):
         
@@ -135,13 +177,42 @@ class Gsbatch():
     def _gsls(self, path):
         
         cmd = f"gsutil ls {path}"
+        print(cmd)
         output = subprocess.check_output(cmd, shell=True)
         output = str(output)[2:-1]
         
         output_split = output.split('\\n')[:-1]
         
         return output_split
+    
+    def _ls(self, path):
         
+        cmd = f"ls {path}"
+        output = subprocess.check_output(cmd, shell=True)
+        output = str(output)[2:-1]
         
+        output_split = output.split('\\n')[:-1]
+        
+        return output_split
+    
+    def _expand_wildcards(self, list_of_paths, search):
+        
+        output_list = []
+        n_str = len(list_of_paths)
+        
+        for ii in range(n_str):
+            path = list_of_paths[ii]
+            if '*' in path:
+                if search == 'remote':
+                    wc_files = self._gsls(path)
+                elif search == 'local':
+                    wc_files = glob.glob(path)
+                if type(wc_files) is not list:
+                    wc_files = [wc_files]
+                output_list = output_list + wc_files
+            else:
+                output_list.append(path)
+                
+        return output_list
             
         
